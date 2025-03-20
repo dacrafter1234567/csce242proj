8b0516377c4f4c41caca3ce71b4ae0ecef4aaa1a
dacrafter1234567 <dacrafter247@gmail.com>
2025-03-20 09:08:46 -0400

    setup assignment 3

---
+++ csce242proj/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello there</h1>
+      <h3>General Kenobi.</h3>
+    </div>
+    
+  );
+}
+
+export default App;
